0905d1e89fb8b1087b824c70be5ff830ac805664
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 14:24:56 -0500

    models and migrations
    working

---
+++ app/models/card.rb
@@ -0,0 +1,5 @@
+class Card < ActiveRecord::Base
+  belongs_to :deck
+  
+  # Remember to create a migration!
+end
---
+++ db/migrate/20130830141213_create_decks.rb
@@ -0,0 +1,7 @@
+class CreateDecks < ActiveRecord::Migration
+  def change
+    create_table :decks do |t|
+      t.string :name
+      t.timestamps
+  end
+end
