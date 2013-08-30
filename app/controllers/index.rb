52cb9d8a2e791a7b35f3b53b30bd6575401ffaa1
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 12:25:51 -0500

    moved to new computer

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,26 @@
+# GET ============================================
+
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  @user = User.find_by_email(params[:email])
+  if @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect to "/games"
+  else
+    erb :index
+  end
+end
+
+get '/register' do
+  erb :register
+end
+
+
+  
+
+# POST ===========================================
+
+post '/register' do
+  @user = User.create(email: params[:email],
+                      password: params[:password])
+  redirect to('/games')
 end
